f6fa53578d267183c3526a2e9fa595078fb7891e
Vladykoo <vladykoo@mail.ru>
2021-07-05 14:50:05 +0300

    add axios plugin

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axiosPlugin from '../plugins/axios'
 
 Vue.config.productionTip = false
+Vue.config.errorHandler = function(err, vm, info) {
+  vm.$errors(err, info)  
+}
+Vue.use(axiosPlugin)
 
 new Vue({
   render: h => h(App)
